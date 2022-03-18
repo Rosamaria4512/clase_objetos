30151b2512bf7d1dbf39a2ffeadadb972e919c92
unknown <rmquilindo8@misena.edu.co>
2022-03-18 16:47:51 -0500

    conexion bd 2

--- conexion a MSQ/objUsuario.php
+++ conexion a MSQ/objUsuario.php
@@ -2,7 +2,29 @@
 require_once("usuario.php");
 
 $objUsuario = new Usuario();
-$insertarUsuario = $objUsuario->insertarUsuario( "laura", "marcelo@misena.edu.co", 123456);
-echo $insertarUsuario;
+//INSERTAR USUARIOS:
+//$insertarUsuario = $objUsuario->insertarUsuario( "laura", "marcelo@misena.edu.co", 123456);
+//echo $insertarUsuario;
+
+
+//modificar usuarios:
+    //$editarusuario = $objUsuario->editarUsuario(4, "valentina ledezma", "vale123@gamil.com", 234423);
+
+
+//ELIMINAR DATOS:
+  $eLiminarUsuario =$objUsuario->eLiminarUsuario(4);
+
+
+
+
+
+//ver la lista de usuarios
+$verUsuarios = $objUsuario->getUsuarios();
+
+
+print_r('<pre>');
+print_r($verUsuarios);
+print_r('</pre>');
+
 
 ?>
--- conexion a MSQ/usuario.php
+++ conexion a MSQ/usuario.php
@@ -19,10 +19,37 @@ public function insertarUsuario($nombre, $email ,$password){
     $idUsuario = $this->db->lastInsertId();
     return $idUsuario;
 }
+public function getUsuarios(){
+    $rows = Null;
+    $tabla = $this->db->prepare("SELECT id, nombre,email, password FROM usuario");
+    $tabla ->execute();
+    while ($result = $tabla->fetch()) {
+        $rows[] =$result;
+    }
+    return $rows;
+    
+    }
+
+public function editarUsuario($id, $nombre, $email, $password){
+    $tabla = $this->db->prepare("UPDATE usuario SET nombre = :nombre, email = :email, password = :password WHERE id= $id");
+
+    $tabla->bindParam(':nombre', $nombre);
+    $tabla->bindParam(':email', $email);
+    $tabla->bindParam(':password',$password);
+    $tabla->execute();
+    return $tabla;
+}
+public function eLiminarUsuario($id){
+    $tabla = $this->db->prepare("DELETE FROM usuario WHERE id= :id");
+    $tabla->bindParam(':id', $id);
+    $tabla->execute();
+    return $tabla;
+    # code...
+
 }
 
 
 
-
+}
 
 ?>
